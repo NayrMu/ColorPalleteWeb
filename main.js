6f129ec083e2bf55fd0bb075a6a0ab29606d14a2
NayrMu <123273762+NayrMu@users.noreply.github.com>
2023-07-28 21:08:00 -0500

    Centered Div :P

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 function randomButton(id) {
-	document.getElementById("colorCanvas").style.backgroundColor = randomColor();
+	color = randomColor();
+	document.getElementById("palleteColor1").style.backgroundColor = color;
+	document.getElementById("palleteColor1Text").innerHTML = color;
 }
 
 function randomColor() {
 	const colorArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"];
 	let color = "#";
 	for (let i = 0; i < 6; i++) {
-		color += colorArr[Math.floor(Math.random() * 15)]
+		color += colorArr[Math.floor(Math.random() * 15)];
 		console.log(color);
 	}
 	console.log(color);
 	return color;
 }
+
