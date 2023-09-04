555cb369718fda1efd37f46110d8702adaa3341b
NayrMu <123273762+NayrMu@users.noreply.github.com>
2023-09-04 00:02:41 -0500

    added a locking ability, subtracting ability, and cleaner aesthetics.

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 let numColors = 0;
-
+const maxColors = 6;
 
 function randomButton() {
 	
 	palleteColors = document.querySelectorAll(".palleteColor");
-	for (var i =0; i < palleteColors.length; i++) {
-		color = randomColor();
-		console.log("is this " + palleteColors[i].style.backgroundColor);
-		palleteColors[i].style.backgroundColor = color;
-		palleteColors[i].innerHTML = color;
-	}
+	palleteColors.forEach((palleteColor) => {
+
+		const isLocked = palleteColor.dataset.isLocked;
+		console.log(isLocked)
+		if (isLocked === "false") {
+			color = randomColor();
+			p = palleteColor.querySelector("p");
+			palleteColor.style.backgroundColor = color;
+			p.innerHTML = color;
+		}
+	});
 }
 
 function randomColor() {
@@ -17,25 +22,64 @@ function randomColor() {
 	let color = "#";
 	for (let i = 0; i < 6; i++) {
 		color += colorArr[Math.floor(Math.random() * 15)];
-		console.log(color);
-	}
-	console.log(color);
+	} 
 	return color;
 }
 
 function addColor() {
 
-	
-	const colorCanvas = document.getElementById("colorCanvas");
+	if (numColors < maxColors) {
+		const colorCanvas = document.querySelector(".colorCanvas");
 
-	const template = document.getElementById("templateColor");
+		const template = document.getElementById("templateColor");
 
-	const clone = template.content.cloneNode(true);
+		const clone = template.content.cloneNode(true);
 
-	const div = clone.querySelector("#palleteColor1")
-	colorCanvas.appendChild(clone);
-	numColors += 1;
+		const div = clone.querySelector("#palleteColor")
+		colorCanvas.appendChild(clone);
+		numColors += 1;
 
-	randomButton();
+		locks = div.querySelectorAll("i");
+
+
+		div.dataset.isLocked = "false";
+
+		locks.forEach((lock) => {
+			lock.addEventListener("click", () => lockFunc(lock, div));
+		});
+		
+		
+
+		randomButton();
+	}
 }
 
+function subtractColor() {
+	const colorCanvas = document.querySelector(".colorCanvas")
+	const palleteColors = document.querySelectorAll(".palleteColor")
+	for (const palleteColor of palleteColors) {
+		if (palleteColor.dataset.isLocked == "false") {
+			colorCanvas.removeChild(palleteColor);
+			break;
+		}
+	}
+	
+	numColors -= 1;
+}
+
+function lockFunc(lock, div) {
+
+		if (lock.classList.contains("fa-lock-open")) {
+			lock.classList.remove("fa-lock-open");
+			lock.classList.add("fa-lock");
+
+		} else { 
+			lock.classList.add("fa-lock-open");
+			lock.classList.remove("fa-lock");
+		}
+		if (div.dataset.isLocked === "false") {
+			div.dataset.isLocked = "true";
+			console.log(div.dataset.isLocked)
+		} else { div.dataset.isLocked = "false"}
+
+}
